6feac791c5c1d3be9ab4d65a647318de4adb177d
Davoud Badamchi <Davoud.badamchi@gmail.com>
2024-07-07 09:54:52 +0330

    removed spring security

--- src/main/java/com/example/getWeatherDataByIpAddressOfUser/config/SecurityConfig.java
+++ src/main/java/com/example/getWeatherDataByIpAddressOfUser/config/SecurityConfig.java
@@ -1,42 +1,42 @@
-package com.example.getWeatherDataByIpAddressOfUser.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
-
-@EnableWebSecurity
-@Configuration
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> {
-                    authorize.requestMatchers(antMatcher("/swagger-ui/**"),
-                                    antMatcher("/swagger-ui.html"),
-                                    antMatcher("/h2-console/**")).permitAll()
-                            .requestMatchers("/api/weather", "/h2-console/**").permitAll()
-                            .anyRequest().authenticated();
-                })
-                .headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)) // This so embedded frames in h2-console are working
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        return httpSecurity.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(8);
-    }
-}
+//package com.example.getWeatherDataByIpAddressOfUser.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+//
+//@EnableWebSecurity
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(authorize -> {
+//                    authorize.requestMatchers(antMatcher("/swagger-ui/**"),
+//                                    antMatcher("/swagger-ui.html"),
+//                                    antMatcher("/h2-console/**")).permitAll()
+//                            .requestMatchers("/api/weather", "/h2-console/**").permitAll()
+//                            .anyRequest().authenticated();
+//                })
+//                .headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)) // This so embedded frames in h2-console are working
+//                .httpBasic(Customizer.withDefaults())
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//
+//        return httpSecurity.build();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder(8);
+//    }
+//}
